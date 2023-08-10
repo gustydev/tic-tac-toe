e46acb43e0ac2f40c05a303f8e8da39b304ad6c5
gusty <gustavodiorlando@gmail.com>
2023-08-10 15:20:03 -0300

    Display gameboard array on page

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-const gameBoard = () => {
-    row1 = ['X', '', 'O'];
-    row2 = ['X', 'O', 'O'];
-    row3 = ['', 'X', ''];
-    const addPlay = 
-}
+const gameBoard = (() => {
+    const rowsDisplay = document.querySelectorAll('div.row');
+    let rows = [['X', 'O', 'X'], ['', 'X', ''], ['O', 'O', '']];
+    rowsDisplay.forEach(row => {
+        for (let i = 0; i < 3; i++) {
+            row.children[i].textContent = rows[row.id][i]
+        }
+    })
+    return {rows};
+})();
+// Arguments should be the 3 rows (updated in "update game" function)
+
