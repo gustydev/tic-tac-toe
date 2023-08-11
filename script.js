2e89e57ee5b82ef58d52db403e2111a21ca359d7
gusty <gustavodiorlando@gmail.com>
2023-08-11 12:11:49 -0300

    Start board clicking for play code

--- script.js
+++ script.js
@@ -1,12 +1,37 @@
-const gameBoard = (() => {
-    const rowsDisplay = document.querySelectorAll('div.row');
-    let rows = [['X', 'O', 'X'], ['', 'X', ''], ['O', 'O', '']];
-    rowsDisplay.forEach(row => {
+const board = (() => {
+    const rows = [[], [], []];
+    const update = () => {    
+        const rowsDisplay = document.querySelectorAll('div.row');
+        rowsDisplay.forEach(row => {
         for (let i = 0; i < 3; i++) {
             row.children[i].textContent = rows[row.id][i]
         }
     })
-    return {rows};
+    }
+    const reset = () => {
+        rows.map((row) => row.splice(0));
+        update();
+    }
+    return {update, rows, reset};
 })();
-// Arguments should be the 3 rows (updated in "update game" function)
 
+const newPlayer = (name) => {
+    const sayName = () => console.log(`Hello there, ${name}!`)
+    return {sayName, name};
+}
+
+// gameBoard.update([['X', '', 'X'], ['O', '', 'O'], ['X', 'O', 'X']]);
+// Temporary, just to have something on the page
+
+const game = (() => {
+    const play = () => {
+        const squares = document.querySelectorAll('div.square');
+        squares.forEach(square => {
+            square.addEventListener('click', () => {
+                board.rows[square.parentElement.id][square.classList[1]] = 'X';
+                board.update();
+            })
+        })
+    }
+    return {play};
+})();
