79dfbdc70f55cd570d9a5fb42c3ee34aa299510d
gusty <gustavodiorlando@gmail.com>
2023-08-14 10:40:46 -0300

    Complete victory check code

--- script.js
+++ script.js
@@ -25,13 +25,71 @@ const newPlayer = (name) => {
 
 const game = (() => {
     const play = () => {
+        let currentPlayer = 'X';
+        const currentDisplay = document.querySelector('div.current-player');
+        const updateDisplay = () => {
+            currentDisplay.textContent = `Player ${currentPlayer}'s turn`;
+        };
+        updateDisplay();
         const squares = document.querySelectorAll('div.square');
         squares.forEach(square => {
             square.addEventListener('click', () => {
-                board.rows[square.parentElement.id][square.classList[1]] = 'X';
+                if (!square.textContent) {
+                    if (currentPlayer === 'X') {
+                        board.rows[square.parentElement.id][square.classList[1]] = 'X';
+                        currentPlayer = 'O'
+                        updateDisplay();
+                    } else {
+                        board.rows[square.parentElement.id][square.classList[1]] = 'O';
+                        currentPlayer = 'X'
+                        updateDisplay();
+                    }
+                }
                 board.update();
+                checkWin();
             })
         })
     }
-    return {play};
+    const checkWin = () => {
+        board.rows.forEach(row => {
+            if (!row.includes(undefined) && row.length === 3) {
+                if (row.every((element) => element === 'X')) {
+                    alert('Player X wins!');
+                    // board.reset();
+                } else if (row.every((element) => element === 'O')) {
+                    alert('Player O wins!');
+                }
+            }
+        })
+        let column = [];
+        for (let i = 0; i < 3; i++) {
+            for (let z = 0; z < 3; z++) {
+                column.push(board.rows[z][i]);
+            }
+            if (column.join('') === 'XXX') {
+                alert('Player X wins!')
+            } else if (column.join('') === 'OOO') {
+                alert('Player O wins!')
+            }
+            column.splice(0);
+        }
+        const diagonals = [[board.rows[0][0], board.rows[1][1], board.rows[2][2]], [board.rows[0][2], board.rows[1][1], board.rows[2][0]]];
+        diagonals.forEach(diagonal => {
+            if (diagonal.join('') === 'XXX') {
+                alert('Player X wins!')
+            } else if (diagonal.join('') === 'OOO') {
+                alert('Player O wins!')
+            }
+        });
+    }
+    return {play, checkWin};
 })();
+
+game.play();
+
+// 0[0] 1[1] 2[2]
+// 0[2] 1[1] 2[0]
+
+// board.rows[0][0], board.rows[1][1], board.rows[2][2] === 'X'
+
+
